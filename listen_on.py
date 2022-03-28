562b5db9109b886a9946855eea5e5850eb7ed9c6
Red Cricket <red.cricket.blog@gmail.com>
2022-03-27 19:15:56 -0700

    lint errors

--- listen_on.py
+++ listen_on.py
@@ -1,21 +1,4 @@
 #!/usr/bin/python3
-
-ANSIBLE_METADATA = {
-    'metadata_version': '1.1',
-    'status': ['preview'],
-    'supported_by': 'community'
-}
-
-DOCUMENTATION = '''
-'''
-
-EXAMPLES = '''
-'''
-
-RETURN = '''
-'''
-
-
 from ansible.module_utils.basic import AnsibleModule
 from daemonize import Daemonize
 import concurrent.futures
@@ -24,6 +7,51 @@ import select
 import socket
 import sys
 import time
+
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: my_test
+
+short_description: This is my test module
+
+# If this is part of a collection, you need to use semantic versioning,
+# i.e. the version is of the form "2.5.0" and not "2.4".
+version_added: "1.0.0"
+
+description: This is my longer description explaining my test module.
+
+options:
+    name:
+        description: This is the message to send to the test module.
+        required: true
+        type: str
+    new:
+        description:
+            - Control to demo if the result of this module is changed or not.
+            - Parameter description can be a list as well.
+        required: false
+        type: bool
+# Specify this value according to your collection
+# in format of namespace.collection.doc_fragment_name
+extends_documentation_fragment:
+    - my_namespace.my_collection.my_doc_fragment_name
+
+author:
+    - Your Name (@yourGitHubHandle)
+'''
+
+EXAMPLES = '''
+'''
+
+RETURN = '''
+'''
+
 
 class ListenOnPort:
 
@@ -36,13 +64,13 @@ class ListenOnPort:
             try:
                 self.timeout = time.time() + int(timeout)
             except Exception as ex:
-                print('error %s' % ex )
+                print('error %s' % ex)
 
     def client_connect(self):
-        HOST = "127.0.0.1"
+        host = "127.0.0.1"
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.connect((HOST, self.port))
+                s.connect((host, self.port))
             return True
         except Exception as ex:
             self.logger.info('Error client_connect() ex=%s.' % ex)
@@ -51,14 +79,14 @@ class ListenOnPort:
     def listen_on_port(self):
         try:
             running = True
-            HOST = ''                # Symbolic name meaning all available interfaces
-            PORT = int(self.port)    # Arbitrary non-privileged port
+            host = ''  # Symbolic name meaning all available interfaces
+            port = int(self.port)  # Arbitrary non-privileged port
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            server_socket.bind((HOST, PORT))
+            server_socket.bind((host, port))
             server_socket.listen()
             server_socket.setblocking(False)  # non-blocking I/O
-            self.logger.info('listening on port %s' % self.port)
+            self.logger.info('listening on port %s' % port)
             read_list = [server_socket]
             while running:
                 if self.timeout:
@@ -91,6 +119,7 @@ class ListenOnPort:
             self.logger.info('Error ex=%s' % ex)
             raise ex
 
+
 def listen_on_port(port, timeout=None):
     # format = "%(asctime)s: %(message)s"
     # logging.basicConfig(format=format, level=logging.INFO, filename='listen_on.log', datefmt="%H:%M:%S")
@@ -101,14 +130,15 @@ def listen_on_port(port, timeout=None):
     fh.setLevel(logging.DEBUG)
     logger.addHandler(fh)
     keep_fds = [fh.stream.fileno()]
-    l = ListenOnPort(port=port, logger=logger, timeout=timeout)
-    if l.client_connect():
+    l_on_p = ListenOnPort(port=port, logger=logger, timeout=timeout)
+    if l_on_p.client_connect():
         print('{"changed": false, "msg": "Already listening on port %s."}' % port)
         return
     print('{"changed": true, "msg": "listening on port %s."}' % port)
     sys.stdout.flush()
-    d = Daemonize(app="demon_%s" % port, pid=l.pid, keep_fds=keep_fds, action=l.listen_on_port)
+    d = Daemonize(app="demon_%s" % port, pid=l_on_p.pid, keep_fds=keep_fds, action=l_on_p.listen_on_port)
     d.start()
+
 
 def run_module():
     # define the available arguments/parameters that a user can pass to
@@ -148,22 +178,23 @@ def run_module():
         # print('got a timeout', flush=True)
         # socket.setdefaulttimeout(module.params.get('listen_on_timeout'))
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            executor.submit(listen_on_port,module.params.get('listen_on_port'), module.params.get('listen_on_timeout'))
+            executor.submit(listen_on_port, module.params.get('listen_on_port'), module.params.get('listen_on_timeout'))
     else:
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            executor.submit(listen_on_port,module.params.get('listen_on_port'))
+            executor.submit(listen_on_port, module.params.get('listen_on_port'))
 
     # use whatever logic you need to determine whether or not this module
     # made any modifications to your target
-    #if module.params['listen_on_port']:
-    #result['changed'] = True
-    #result['msg'] = "listing on port %s" % module.params['listen_on_port']
-    #print("exitting")
-    #module.exit_json(**result)
-    
+    # if module.params['listen_on_port']:
+    # result['changed'] = True
+    # result['msg'] = "listing on port %s" % module.params['listen_on_port']
+    # print("exitting")
+    # module.exit_json(**result)
+
 
 def main():
     run_module()
+
 
 if __name__ == '__main__':
     main()
